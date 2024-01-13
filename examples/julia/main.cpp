5413df8c25cf3117749f08896be27de5820f873f
hhubibi <hxynobibi@gmail.com>
2024-01-13 20:20:57 +0800

    modify julia

--- examples/julia/main.cpp
+++ examples/julia/main.cpp
@@ -19,12 +19,7 @@ float c_y = 0.355;
 
 void cuda_draw_julia(uchar3* ptr, int width, int height, float scale, int max_iter, float c_x, float c_y);
 
-void draw_julia(int width, int height) {
-    glLoadIdentity();
-    glOrtho(0, width, 0, height, -1, 1);
-
-    glBegin(GL_POINTS);
-
+void draw_julia(std::vector<GLubyte>& pixels, int width, int height) {
     for (int x = 0; x < width; ++x) {
         for (int y = 0; y < height; ++y) {
             std::complex<float> a(scale * (float)(width/2 - x) / (width/2), scale * (float)(height/2 - y) / (height/2));
@@ -39,12 +34,13 @@ void draw_julia(int width, int height) {
             }
 
             int flag = (i == max_iter) ? 1 : 0;
-            glColor3f(255 * flag, 0, 0);
-            glVertex2i(x, y);
+            size_t index = (x + y * width) * 3;
+            pixels[index] = 255*flag;
+            pixels[index+1] = 0;
+            pixels[index+2] = 0;
         }
     }
 
-    glEnd();
 }
 
 void cpu_draw() {
@@ -59,6 +55,20 @@ void cpu_draw() {
     }
 
     glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        throw std::runtime_error("failed to init glew");
+    }
+
+    std::vector<GLubyte> pixels(width * height * 3);
+
+    GLuint buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, buffer);
+    glBufferData(GL_PIXEL_UNPACK_BUFFER, width*height*3, nullptr, GL_DYNAMIC_DRAW);
+
     double last_time = glfwGetTime();
     int frame_count = 0;
 
@@ -74,12 +84,17 @@ void cpu_draw() {
 
         glClear(GL_COLOR_BUFFER_BIT);
 
-        draw_julia(width, height);
+        draw_julia(pixels, width, height);
+
+        glBufferSubData(GL_PIXEL_UNPACK_BUFFER, 0, width*height*3, pixels.data());
+        glDrawPixels(width, height, GL_RGB, GL_UNSIGNED_BYTE, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+    glDeleteBuffers(1, &buffer);
     glfwDestroyWindow(window);
     glfwTerminate();
 }
@@ -108,13 +123,6 @@ void cuda_draw() {
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_PIXEL_UNPACK_BUFFER, buffer);
     glBufferData(GL_PIXEL_UNPACK_BUFFER, width*height*3, nullptr, GL_DYNAMIC_DRAW);
-
-    glViewport(0, 0, width, height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0, width, height, 0, -1, 1);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
 
     cudaGraphicsResource *cuda_resource;
     cudaError_t cuda_status = cudaGraphicsGLRegisterBuffer(&cuda_resource, buffer, cudaGraphicsMapFlagsNone);
@@ -185,18 +193,17 @@ int main(int argc, char** argv) {
         {"width", 1, 0, 'w'},
         {"height", 1, 0, 'h'},
         {"scale", 1, 0, 's'},
-        {"cx", 1, 0, 256},
-        {"cy", 1, 0, 257},
+        {"cx", 1, 0, 'x'},
+        {"cy", 1, 0, 'y'},
         {0, 0, 0, 0}
     };
 
-    while ((opt = getopt_long(argc, argv, "w:h:s:r:?", long_options, nullptr)) != EOF) {
+    bool use_cuda = false;
+    while ((opt = getopt_long(argc, argv, "w:h:s:x:y:r:?", long_options, nullptr)) != EOF) {
         switch (opt) {
         case 'r':
             if (std::string(optarg).compare("gpu") == 0) {
-                cuda_draw();   
-            } else {
-                cpu_draw();
+                use_cuda = true; 
             }
             break;
         case 'h':
@@ -208,10 +215,10 @@ int main(int argc, char** argv) {
         case 's':
             scale = std::atof(optarg);
             break;
-        case 256:
+        case 'x':
             c_x = std::atof(optarg);
             break;
-        case 257:
+        case 'y':
             c_y = std::atof(optarg);
             break;
         case '?':
@@ -219,6 +226,12 @@ int main(int argc, char** argv) {
             usage(argv[0]);
             return 1;
         }
+    }
+
+    if (use_cuda) {
+        cuda_draw();
+    } else {
+        cpu_draw();
     }
 
     return 0;
